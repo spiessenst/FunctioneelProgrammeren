daffa745b45a2009bfbe463f216f30339a07aa9c
spiessenst <spiessenst@gmail.com>
2021-12-12 15:54:46 +0100

    update php shorthand

--- oef3.1/stad_form.php
+++ oef3.1/stad_form.php
@@ -26,37 +26,38 @@ print PrintJumbo("Bewerk Afbeelding" , "");
 
 <div class="container">
 
+    <?php $stad = $steden[0]; ?>
 
     <form id="mainform" name="mainform" action="save.php " method="post">
         <div class="form-group row">
             <label for="id" class="col-sm-2 col-form-label">ID</label>
             <div class="col-sm-10">
-                <input type="number" readonly class="form-control-plaintext" name="img_id" id="id" value="<?php print $steden[0]['img_id'];  ?>">
+                <input type="number" readonly class="form-control-plaintext" name="img_id" id="id" value="<?=$stad['img_id'];  ?>">
             </div>
         </div>
         <div class="form-group row">
             <label for="title" class="col-sm-2 col-form-label">Tilte</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="img_title" id="title" value="<?php print $steden[0]['img_title'];  ?>">
+                <input type="text" class="form-control" name="img_title" id="title" value="<?=$stad['img_title'];  ?>">
             </div>
         </div>
         <div class="form-group row">
             <label for="filename" class="col-sm-2 col-form-label">Filename</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="img_filename" id="filename" value="<?php print $steden[0]['img_filename'];  ?>">
+                <input type="text" class="form-control" name="img_filename" id="filename" value="<?=$stad['img_filename'];  ?>">
             </div>
         </div>
 
         <div class="form-group row">
             <label for="width" class="col-sm-2 col-form-label">Width</label>
             <div class="col-sm-10">
-                <input type="number" class="form-control" name="img_width" id="width"value="<?php print $steden[0]['img_width'];  ?>">
+                <input type="number" class="form-control" name="img_width" id="width"value="<?=$stad['img_width'];  ?>">
             </div>
         </div>
         <div class="form-group row">
             <label for="height" class="col-sm-2 col-form-label">Height</label>
             <div class="col-sm-10">
-                <input type="number" class="form-control"  name="img_height" id="height" value="<?php print $steden[0]['img_height'];  ?>">
+                <input type="number" class="form-control"  name="img_height" id="height" value="<?=$stad['img_height'];  ?>">
             </div>
         </div>
         <div class="form-group row">
@@ -69,7 +70,7 @@ print PrintJumbo("Bewerk Afbeelding" , "");
 <div class="form-group row">
             <div class="col-sm-10">
               <?php
-                print "<img src=./images/". $steden[0]['img_filename']." display=block width=100% height=auto >";
+                print "<img src=./images/". $stad['img_filename']." display=block width=100% height=auto >";
                 print "<a href=steden.php>Terug naar overzicht</a>";
                 ?>
             </div>
