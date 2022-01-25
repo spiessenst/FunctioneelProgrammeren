1b25f5c68efa7981315c395bba6d29e1ad73be63
spiessenst <spiessenst@gmail.com>
2022-01-25 14:49:38 +0100

    login.php

--- oef4.2/lib/login_check.php
+++ oef4.2/lib/login_check.php
@@ -16,11 +16,10 @@ function LoginCheck()
     $validUser = $validPass = "";
 
 
-    if ( $_SERVER['REQUEST_METHOD'] == "POST" )
-    {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
         //controle CSRF token
-        if ( ! key_exists("csrf", $_POST)) die("Missing CSRF");
-        if ( ! hash_equals( $_POST['csrf'], $_SESSION['lastest_csrf'] ) ) die("Problem with CSRF");
+        if (!key_exists("csrf", $_POST)) die("Missing CSRF");
+        if (!hash_equals($_POST['csrf'], $_SESSION['lastest_csrf'])) die("Problem with CSRF");
 
         $_SESSION['lastest_csrf'] = "";
 
@@ -31,30 +30,19 @@ function LoginCheck()
         $rows = GetData($sql);
 
 
-        if (  key_exists("usr_email", $_POST)) {
-
+        if (key_exists("usr_email", $_POST)) {
             if ($rows > 0) $validUser = true;
             else $validUser = false;
         }
+        if (key_exists("usr_password", $_POST)) {
 
-            if (  key_exists("usr_password", $_POST)) {
+            if (password_verify($_POST['usr_password'], $rows[0]['usr_password'])) $validPass = true;
+            else $validPass = false;
+        }
 
-                if (password_verify($_POST['usr_password'], $rows[0]['usr_password'])) {
-                    $validPass = true;
-                }else{
-                    $validPass = false;
-                }
+        if ($validUser == true && $validPass == true) return true;
 
-            }
-
-if( $validUser == true && $validPass== true){
-    return true;
-}else {
-    return false;
 }
-
-    }
 }
-
 
 
